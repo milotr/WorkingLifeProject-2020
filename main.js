4f54e52e05d841f10e6c004f6705d0ab31d5448d
Viktor <47790760+Paganea@users.noreply.github.com>
2020-08-28 09:48:44 +0300

    Added comments on code

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 
 function initMap() {
-  // Create the map.
+// Function  to create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     center: {lat: 64, lng: 26},
   });
  
 
-   // Load the train GeoJSON onto the map.
+// Load the train GeoJSON onto the map.
 $.getJSON('https://rata.digitraffic.fi/api/v1/train-locations.geojson/latest',function (data) {
 features = map.data.addGeoJson(data);
-//window.
-  //map.data.loadGeoJson('https://rata.digitraffic.fi/api/v1/train-locations.geojson/latest', {idPropertyName: 'trainNumber'});
   google.maps.event.addDomListener(document.getElementById('map'), 'load', setInterval(function () {
+// Update each train marker
   for (var i = 0; i < features.length; i++)
 	{
 	map.data.remove(features[i]);
 	}
-	 //map.data.loadGeoJson('https://rata.digitraffic.fi/api/v1/train-locations.geojson/latest',
 	features = map.data.addGeoJson(data);
 	},149999));
-	//1000)
+// Assign logo and other features for each train marker
  var trainicon = {
     url: 'train.png',
 };
@@ -28,19 +26,24 @@ map.data.setStyle({
     icon: trainicon,
 	animation: google.maps.Animation.DROP,
 	});
+// Create infowindow for each train marker when you click in it
 const apiKey = 'AIzaSyBty_Ly3P5yJTnroKVUU7QHiocHKmbsonA';
   const infoWindow = new google.maps.InfoWindow();
   
   map.data.addListener('click', (event) => {
-    const trainNumber = event.feature.getProperty('trainNumber');
+// Get data for infowindow
+	const trainNumber = event.feature.getProperty('trainNumber');
     const departureDate = event.feature.getProperty('departureDate');
     const speed = event.feature.getProperty('speed');
     const position = event.feature.getGeometry().get();
-	   const content = `
+// Spacify data infowindow formation
+	const content = `
       <h2><b>Train:</b> ${trainNumber}</h2><p><b>Departure date:</b> ${departureDate}</p>
       <p><b>Speed:</b> ${speed} km/h<br/>
     `;
+// Set data at infowindow
 	infoWindow.setContent(content);
+// Show infowindow at aprropriate place(where train location is)
 	infoWindow.setPosition(position);
     infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
     infoWindow.open(map);
